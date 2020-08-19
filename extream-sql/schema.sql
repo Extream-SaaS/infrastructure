22a543ccc9b6fad0f49c95687df98c04df46e4c3
Rich Wilson <richard.wilson@smgdesign.org>
2020-08-19 19:18:46 +0100

    updated schema

--- extream-sql/schema.sql
+++ extream-sql/schema.sql
@@ -32,3 +32,45 @@ WITH (
 
 ALTER TABLE public.access_tokens
     OWNER to postgres;
+
+DROP TABLE if EXISTS public.organisations;
+
+CREATE TABLE public.organisations
+(
+    id serial,
+    public_id uuid DEFAULT uuid_generate_v4 (),
+    name text,
+    website text,
+    user_id integer,
+    parent integer,
+    landing_page integer,
+    PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.organisations
+    OWNER to postgres;
+
+DROP TABLE if EXISTS public.events;
+
+CREATE TABLE public.events
+(
+    id serial,
+    public_id uuid DEFAULT uuid_generate_v4 (),
+    name text,
+    website text,
+    organisation_id integer,
+    start_date timestamp,
+    end_date timestamp,
+    parent integer,
+    landing_page integer,
+    PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.events
+    OWNER to postgres;
--- readme.md
+++ readme.md
@@ -6,6 +6,16 @@ Export the location to the default service account: eg: `export GOOGLE_APPLICATI
 
 https://github.com/GoogleCloudPlatform/nodejs-docs-samples/tree/master/cloud-sql/postgres/knex
 
+Using a port:
+
 `./cloud_sql_proxy -instances=stoked-reality-284921:europe-west2:extream=tcp:5432`
 
 Test the connection: `psql "host=127.0.0.1 port=5432 sslmode=disable dbname=ex-manage user=postgres"`
+
+Using a socket:
+
+`mkdir /cloud_sql && chmod 777 /cloud_sql`
+
+`./cloud_sql_proxy -instances=stoked-reality-284921:europe-west2:extream -dir=/cloud_sql &`
+
+Test the connection: `psql "host=/cloud_sql/stoked-reality-284921:europe-west2:extream sslmode=disable dbname=ex-manage user=postgres"`
